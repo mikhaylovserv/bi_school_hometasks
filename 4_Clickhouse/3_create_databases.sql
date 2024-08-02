aa5283f7f2e7790e122a02cf6142e59c4f9f6410
mikhaylovserv <mikhaylovserv@gmail.com>
2024-08-02 14:16:48 +0300

    Create 3_create_databases.sql

---
+++ 4_Clickhouse/3_create_databases.sql
@@ -0,0 +1,4 @@
+create database stage; --база стэйджинга
+create database history; --база историческая
+create database current; --база текущих данных
+create database direct_log; --база буферных таблиц
